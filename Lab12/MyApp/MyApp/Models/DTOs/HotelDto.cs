9da33a11917df5481c8ed3bb73db954f4b359a5f
shadilios <70282602+shadilios@users.noreply.github.com>
2022-04-27 20:25:20 +0300

    DTOs initial

---
+++ Lab12/MyApp/MyApp/Models/DTOs/HotelDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace MyApp.Models.DTOs
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int Phone { get; set; }
+        
+        public List<RoomDto> Rooms { get; set; }
+    }
+}
